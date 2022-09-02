286ca44a122e007ea969346f6dba0481c6127e41
Dmitry <mandrin2013@mail.ru>
2022-09-02 17:16:09 +0300

    commit

--- src/js/modules/playVideo.js
+++ src/js/modules/playVideo.js
@@ -1,5 +1,15 @@
 export default class VideoPlayer {
-   constructor(truggers, overlay) {
-      
+   constructor(triggers, overlay) {
+      this.btns = document.querySelectorAll(triggers);
+      this.overlay = document.querySelector(overlay);
+      this.close = this.overlay.querySelector('.close');
+   }
+
+   play() {
+      this.btns.forEach(btn => {
+         btn.addEventListener('click', () => {
+            console.log('hello');
+         })
+      })
    }
 }
